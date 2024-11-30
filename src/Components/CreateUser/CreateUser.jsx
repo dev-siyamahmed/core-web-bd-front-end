c226201f4c650e320c4ab35b500ad41bd426f4f8
siyam79 <siyamahmed3827@gmail.com>
2024-11-30 16:48:38 +0600

    api url change

--- src/Components/CreateUser/CreateUser.jsx
+++ src/Components/CreateUser/CreateUser.jsx
@@ -77,7 +77,7 @@ export default function CreateUser() {
           <div className="mb-6 relative">
             <label htmlFor="password" className="block text-md font-medium text-gray-600">Password</label>
             <input
-              type={passwordVisible ? 'text' : 'password'}
+              type={passwordVisible ? 'text' : 'password'} 
               id="password"
               name="password"
               value={password}
@@ -101,7 +101,7 @@ export default function CreateUser() {
           <button
             type="submit"
             className={`w-full bg-indigo-600 text-white p-3 rounded-lg mt-4 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300 ease-in-out ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
-            disabled={loading}
+            disabled={loading} 
           >
             {loading ? 'Creating Account...' : 'Create Account'}
           </button>
